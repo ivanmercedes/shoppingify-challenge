e26e92d6385397b068c5e7c1a9cdf7466f5e768a
Ivan Mercedes <53806989+ivanmercedes@users.noreply.github.com>
2022-03-27 20:57:18 -0400

    feat: connecting resolvers to db

--- backend/models/graphql/resolvers.js
+++ backend/models/graphql/resolvers.js
@@ -1,17 +1,15 @@
-const products = [
-  {
-    id: 1,
-    name: "platano",
-    category: "Vivere",
-    description: "String",
-    image: "imagen test",
-  },
-];
+const Category = require("../Category");
+const Product = require("../Product");
 
 const resolvers = {
   Query: {
     allProducts: async (root, args) => {
+      const products = await Product.find().lean();
       return products;
+    },
+    allCategory: async (root, args) => {
+      const category = await Category.find().lean();
+      return category;
     },
   },
 };
